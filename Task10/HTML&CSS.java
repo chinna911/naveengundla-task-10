c7f1accca15aced5cf2172b00d908867c48cbea4
chinna911 <60340976+chinna911@users.noreply.github.com>
2020-04-07 20:14:39 +0530

    HTML CSS
    html&css.java

---
+++ Task10/HTML&CSS.java
@@ -0,0 +1,118 @@
+body
+{
+margin: 0;
+padding: 20px;
+}
+.sign-in-form
+{
+width: 350px;
+box-shadow: 0 0 3px 0 rgba(0,0,0,0.3);
+background: #fff;
+padding: 40px;
+margin: 8% auto 0;
+text-align: center;
+}
+.input-box
+{
+font-family:"Roboto",sans-serif;
+outline: 1;
+background: #f2f2f2;
+width: 100%;
+border: 0;
+padding: 20px;
+margin: 10px 0px 5px 50px;
+box-sizing: border-box;
+font-size: 14px;
+text-align: center;
+placeholfer: center;
+}
+button 
+{
+
+font-family:"Roboto",sans-serif;
+text-transform: lowercase;
+outline: 0;
+background: #00CED1;
+width: 100%;
+height: 50px;
+border: 0;
+padding: 15px;
+color: #FFFFFF;
+font-size: 18pt;
+}
+.button1
+{
+background-color:blue;
+text-transform: italic;
+}
+
+.button2
+{
+background-color: orange;
+text-transform: italic;
+}
+.button3
+{
+background-color: greenyellow;
+text-transform: italic;
+}
+h2
+{
+
+    background-color: #00CED1;
+    bo
+}
+[07/04, 7:42 PM] Naveen gundla: <html>
+<head>
+<title>EPAM</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<link rel="shortcut icon" href="dowmloads/favicon.ico ">
+</head>
+<body>
+
+<div class="sign-in-form">
+<form>
+    <h2>
+    <table cellpadding="9px"  cellspacing="5px" style="width: 120%; height: 30px;">
+        <tr>
+    <td><img src="https://img.icons8.com/ios-filled/40/000000/education.png"/></td>
+<td>SIGNIN</td>
+<td><img src="https://img.icons8.com/ios-glyphs/40/000000/xbox-x.png"/></td>
+</tr>
+</table>
+</h2>
+
+<table>
+<tr>
+<td> <input type="email" class="input-box" placeholder="email@example.com"></td> 
+<td> <i class="fa fa-envelope fa-2x cust" aria-hidden="true"></i></td> 
+</tr>
+<tr>
+    <td><input type="password" class="input-box" placeholder="password"></td> 
+    <td><i class="fa fa-unlock-alt fa-2x cust" aria-hidden="true"></i></td> 
+
+</tr>
+</table>
+
+</form>
+<button class="button">sign in</button>
+<br>
+<br><a href="">Forgot your password?</a>&nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp 
+<a href="">register?</a><br>
+
+<hr>
+<p class="or">OR</p>
+<button class="button button1" onclick="window.location.href='https://www.facebook.com/'"  >f</button>
+<br>
+<br>
+<button class="button button2"onclick="window.location.href='https://www.google.com/'" >g</button>
+<br>
+<br>
+<button class="button button3">EPAM LOGIN </button>
+</div>
+
+</body>
+</html>
